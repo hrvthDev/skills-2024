2238804d988b264af5caad4a1249ebba1c9c89ca
hrvth-dev <hrvth.dev@gmail.com>
2026-02-15 13:17:07 +0100

    Projekt inicializálása.

---
+++ js/data.mjs
@@ -0,0 +1,32 @@
+export let datas = [
+    {
+        name: "Balaton",
+        image: "../js2024-s17-hu/my-project/forras/img/balaton.jpg",
+        description: "A Balaton (költői nevén: „a magyar tenger”, becenevén: Balcsi, németül: Plattensee, latinul: Lacus Pelso, horvátul: Blatno jezero) tó a Dunántúlon, Közép-Európa legnagyobb tava, Magyarország vízrajzának meghatározó eleme. Könnyen felmelegedő, sekély vize kiválóan alkalmassá teszi a fürdésre és sportolásra, élővilága rendkívül gazdag, a táj változatos vulkanikus kúpjaival sok tekintetben egyedi."
+    },
+    {
+        name: "Duna - Dunakanyar",
+        image: "../js2024-s17-hu/my-project/forras/img/dunakanyar.jpg",
+        description: "A Duna Európa második leghosszabb folyama az oroszországi Volga után. Németországban, a Fekete-erdőben ered két kis patak, a Breg és a Brigach összefolyásával Donaueschingennél, és innen délkeleti irányban 2850 kilométert tesz meg a Fekete-tengerig. Magyarország egész területe e folyam vízgyűjtőjén terül el, itteni főágának hossza 417 km, ezért az ország vízrajzának meghatározó alkotóeleme."
+    },
+    {
+        name: "Tisza-tó",
+        image: "../js2024-s17-hu/my-project/forras/img/tiszato.jpg",
+        description: "A Tisza-tó (1988-ig Kiskörei víztározó) Magyarország második legnagyobb tava és legnagyobb mesterséges tava a Tiszán, az Alföld északi részén. Létrehozásának legfontosabb okai a Kiskörei Vízerőmű működéséhez szükséges egyenletes vízhozam biztosítása volt, valamint az ugyanebben az időszakban, Tiszaújvárosban telepített új Tiszai Hőerőmű működéséhez szükséges magas vízszint biztosítása duzzasztással."
+    },
+    {
+        name: "Tisza",
+        image: "../js2024-s17-hu//my-project/forras/img/tisza.jpg",
+        description: "A Tisza (németül Theiß, szlovákul és románul Tisa, szerbül Тиса / Tisa, ukránul Тиса) a Duna leghosszabb mellékfolyója, Közép-Európa legfontosabb folyóinak egyike, Magyarország második legnagyobb folyója. A Fekete-Tisza és a Fehér-Tisza összefolyásából keletkezik Ukrajnában, Kárpátalján, Rahó közelében, majd érinti Románia, Magyarország, Szlovákia és Szerbia területét. Ez a folyó képez határvonalat Bácska és Bánát között, mielőtt a Vajdaság közepén, Titelnél a Dunába ömlik."
+    },
+    {
+        name: "Zala folyó",
+        image: "../js2024-s17-hu/my-project/forras/img/zala.jpg",
+        description: "A Zala teljes terjedelmében Magyarország területén fekvő folyó. Fő tápláló vízfolyása a Balatonnak, vízgyűjtő területe a tó teljes vízgyűjtő területének 45%-a. Magyarország tizenkettedik legbővebb vizű felszíni vízfolyása a Duna, Tisza, Dráva, Mura, Maros, Rába, Hármas-Körös, Szamos, Bodrog, Sajó, és a Zagyva után. Nyugat-Magyarországon, Vas vármegyében, az Őrségben, Szalafő határában ered (tulajdonképp a Fekete-tóból). A 126 km hosszúságú, 6–15 m³/s átlagos vízhozamú folyó első szakasza keleti irányú. "
+    },
+    {
+        name: "Rába",
+        image: "../js2024-s17-hu/my-project/forras/img/raba.jpg",
+        description: "A Rába (németül Raab, latinul Arrabo) Magyarország 3. leghosszabb folyója, a Duna legjelentősebb magyarországi mellékfolyója. Ausztriában, Stájerországban, a Fischbachi-Alpokban, a Hochlantsch délkeleti lejtőjén ered 1200 m magasságban. Ausztriában 95 km-t tesz meg déli-délkeleti irányban. Gyanafalvánál (Jennersdorf) eléri legdélebbi pontját majd keleti-északkeleti irányban folyik tovább és Alsószölnöknél, 288 m tengerszint feletti magasságban belép Magyarország területére. Egyes szakaszai innentől az osztrák-magyar határt képezi majd Rábatótfalutól (Szentgotthárd) végleg csak magyar területen halad tovább. Alsószölnöktől 188 km megtétele után Győrnél a Mosoni-Dunába ömlik."
+    },
+];
